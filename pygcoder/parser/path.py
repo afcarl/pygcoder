bee374526d3dc431f08bc877f4b8b3ea7bf21696
Brett Graham <brettgraham@gmail.com>
2015-01-02 07:57:57 -0500

    path transform baking working

--- pygcoder/parser/path.py
+++ pygcoder/parser/path.py
@@ -1,22 +1,38 @@
 #!/usr/bin/env python
 
 import copy
+import math
 
-import affine
 import svg.path
 
 from . import transform
 
 
 def transform_point(t, p):
-    np = t * (p.real, p.imag)
-    return np[0] + np[1] * 1j
+    np = t * [[p.real, ], [p.imag, ], [1, ]]
+    return float(np[0, 0]) + float(np[1, 0]) * 1j
+
+
+def transform_radius(t, r):
+    # http://stackoverflow.com/questions/5149301/baking-transforms-into-svg-path-element-commands
+    # only works for uniform scaling
+    # maybe copy from here: https://gist.github.com/timo22345/9413158
+    sx = math.sqrt(
+        float(t[0, 0]) * float(t[0, 0]) + float(t[0, 1]) * float(t[0, 1]))
+    sy = math.sqrt(
+        float(t[1, 0]) * float(t[1, 0]) + float(t[1, 1]) * float(t[1, 1]))
+    return r.real * sx + r.imag * sy * 1j
+
+
+def transform_rotation(t, r):
+    rot = math.atan2(float(t[1, 0]), float(t[1, 1])) * 180. / math.pi
+    return r + rot
 
 
 def transform_path(path, transforms):
     if len(transforms) == 0:
         return path
-    t = transform.resovle(*transforms)
+    t = transform.resolve(*transforms)
     # TODO
     # apply transforms to path
     new_path = svg.path.Path()
@@ -24,22 +40,27 @@ def transform_path(path, transforms):
         ce = copy.copy(e)
         # monkey patch class
         ce.tpoint = lambda v, t=t, self=ce: transform_point(t, self.point(v))
-        #if isinstance(svg.path.Line):
-        #    ce.start = transform_point(t, e.start)
-        #    ce.end = transform_point(t, e.end)
-        #elif isinstance(svg.path.CubicBezier):
-        #    ce.start = transform_point(t, e.start)
-        #    ce.end = transform_point(t, e.end)
-        #    ce.control1 = transform_point(t, e.control1)
-        #    ce.control2 = transform_point(t, e.control2)
-        #elif isinstance(svg.path.QuadraticBezier):
-        #    ce.start = transform_point(t, e.start)
-        #    ce.end = transform_point(t, e.end)
-        #    ce.control = transform_point(t, e.control)
-        #elif isinstance(svg.path.Arc):
-        #    ce.start = transform_point(t, e.start)
-        #    ce.end = transform_point(t, e.end)
-        #    # how to scale radius??
+        if isinstance(e, svg.path.Line):
+            ce.start = transform_point(t, e.start)
+            ce.end = transform_point(t, e.end)
+        elif isinstance(e, svg.path.CubicBezier):
+            ce.start = transform_point(t, e.start)
+            ce.end = transform_point(t, e.end)
+            ce.control1 = transform_point(t, e.control1)
+            ce.control2 = transform_point(t, e.control2)
+        elif isinstance(e, svg.path.QuadraticBezier):
+            ce.start = transform_point(t, e.start)
+            ce.end = transform_point(t, e.end)
+            ce.control = transform_point(t, e.control)
+        elif isinstance(e, svg.path.Arc):
+            ce.start = transform_point(t, e.start)
+            ce.end = transform_point(t, e.end)
+            # how to transform radius?? ce.radius (complex)
+            ce.radius = transform_radius(t, e.radius)
+            # how to transform rotation?? ce.rotation
+            ce.rotation = transform_rotation(t, e.rotation)
+        else:
+            raise Exception("Unknown type: %s" % type(e))
         new_path.append(ce)
     # return modified path
     return new_path
@@ -59,23 +80,30 @@ def from_svg(element):
         if 'transform' in parent.attrib:
             pts = transform.from_string(parent.attrib['transform'])
             ts = pts + ts
-        parent = element.getparent()
+        parent = parent.getparent()
     if len(ts) == 0:
         return path
     # transform path
     return transform_path(path, ts)
 
 
-def center(path):
+def center(path, min_points=100, max_error=0.0001):
     pass
 
 
 def offset(path, radius):
+    # get offset code from here:
+    # https://github.com/Pomax/bezierinfo/blob/gh-pages/framework/BezierCurve.pde
     pass
 
 
 def curves_to_lines(path):
     # turn all beziers into lines/arcs
+    # algorithm:
+    # - bisect: check error (do this a couple times)
+    # - if error > threshold, bisect again on 'bad' side(s)
+    # check error for both arc and line
+    # fit arc by solving linear system from points
     pass
 
 
--- pygcoder/parser/transform.py
+++ pygcoder/parser/transform.py
@@ -2,27 +2,60 @@
 
 import re
 
-import affine
+import numpy
+# affine appears to have a major bug and incorrectly applies
+# matrices to points, so... use numpy
 
 
 match_string = '([a-zA-Z]+)\(([0-9,.\- ]+)\)'
 
 
-def rotate(angle, px=None, py=None):
-    if px is None:
-        pivot = None
-    else:
-        pivot = (px, py)
-    return affine.Affine.rotation(angle, pivot)
+def matrix(a, b, c, d, e, f):
+    return numpy.matrix([[a, c, e], [b, d, f], [0., 0., 1.]])
+
+
+def translate(x, y):
+    return numpy.matrix([[1., 0., x], [0., 1., y], [0., 0., 1.]])
+
+
+def scale(x, y=1.):
+    return numpy.matrix([[x, 0., 0.], [0., y, 0.], [0., 0., 1.]])
+
+
+def rotate(a, x=0., y=0.):
+    ra = numpy.radians(a)
+    r = numpy.matrix([
+        [numpy.cos(ra), -numpy.sin(ra), 0.],
+        [numpy.sin(ra), numpy.cos(ra), 0.],
+        [0., 0., 1.]])
+    if x == 0. and y == 0.:
+        return r
+    return translate(-x, -y) * r * translate(x, y)
+
+
+def skew_x(a):
+    ra = numpy.radians(a)
+    return numpy.matrix([
+        [1., numpy.tan(ra), 0.],
+        [0., 1., 0.],
+        [0., 0., 1.]])
+
+
+def skew_y(a):
+    ra = numpy.radians(a)
+    return numpy.matrix([
+        [1., 0., 0.],
+        [numpy.tan(ra), 1., 0.],
+        [0., 0., 1.]])
 
 
 transforms = {
-    'matrix': affine.Affine,
-    'translate': lambda x, y=0.: affine.Affine.translation(x, y),
-    'scale': affine.Affine.scale,
+    'matrix': matrix,
+    'translate': translate,
+    'scale': scale,
     'rotate': rotate,
-    'skewX': affine.Affine.shear,
-    'skewY': lambda v: affine.Affine.shear(0, v),
+    'skewX': skew_x,
+    'skewY': skew_y,
 }
 
 
@@ -35,10 +68,15 @@ def to_args(s):
 
 def from_string(s):
     """Returns a list of transforms"""
-    tss = re.findall(s, match_string)
+    tss = re.findall(match_string, s)
     return [transforms[tn](*to_args(ts)) for tn, ts in tss]
 
 
 def resolve(*transforms):
+    if len(transforms) == 1:
+        return transforms[0]
+    i = numpy.matrix([[1., 0., 0.], [0., 1., 0.], [0., 0., 1.]])
+    if len(transforms) == 0:
+        return i
     rev = lambda a, b: b * a
-    return reduce(rev, transforms, affine.identity)
+    return reduce(rev, transforms, i)
